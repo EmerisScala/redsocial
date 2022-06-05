be183818b764b34285462e699baac7a020af3db7
Emeris Hernandez <emerisortiz@gmail.com>
2022-06-04 22:24:48 -0500

    Add Hystrix, OpenFeign

--- entity-service-usuarios-v1/src/main/java/com/redsocial/entityserviceusuariosv1/EntityServiceUsuariosV1Application.java
+++ entity-service-usuarios-v1/src/main/java/com/redsocial/entityserviceusuariosv1/EntityServiceUsuariosV1Application.java
@@ -2,8 +2,16 @@ package com.redsocial.entityserviceusuariosv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableEurekaClient
 public class EntityServiceUsuariosV1Application {
 
 	public static void main(String[] args) {
