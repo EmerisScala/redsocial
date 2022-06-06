2a38aa1d12688c8900a26701394c0b74be4bd019
Emeris Hernandez <emerisortiz@gmail.com>
2022-06-05 23:13:59 -0500

    Metricas centralizadas con spring boot admin

---
+++ arquitectura-monitor-server/src/test/java/com/redsocial/arquitecturamonitorserver/ArquitecturaMonitorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redsocial.arquitecturamonitorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaMonitorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
