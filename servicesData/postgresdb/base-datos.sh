49e3bdcd58e9c96a5f748a67da0b80bbd8b41928
Emeris Hernandez <emerisortiz@gmail.com>
2022-06-06 19:12:43 -0500

    Dockerizando microservicios

--- servicesData/postgresdb/base-datos.sh
+++ servicesData/postgresdb/base-datos.sh
@@ -14,9 +14,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE autorizaciones OWNER admin;
     GRANT ALL PRIVILEGES ON DATABASE autorizaciones TO admin;
 
-    CREATE DATABASE amigos OWNER admin;
-    GRANT ALL PRIVILEGES ON DATABASE amigos TO admin;
-
     CREATE DATABASE contratos OWNER admin;
     GRANT ALL PRIVILEGES ON DATABASE contratos TO admin;
 
